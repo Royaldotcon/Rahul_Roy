794e504bf9f817cad14535cebd26dd6cd1f0b467
Royaldotcon <rr6216009@gmail.com>
2025-05-12 16:50:38 +0530

    Add files via upload

---
+++ components/main/hero.tsx
@@ -0,0 +1,18 @@
+import { HeroContent } from "@/components/sub/hero-content";
+
+export const Hero = () => {
+  return (
+    <div className="relative flex flex-col h-full w-full">
+      <video
+        autoPlay
+        muted
+        loop
+        className="rotate-180 absolute top-[-360px] left-0 w-full h-full object-cover -z-20"
+      >
+        <source src="/videos/blackhole.mp4" type="video/mp4" />
+      </video>
+
+      <HeroContent />
+    </div>
+  );
+};
