68382cc24226b3ec693ead3b417a016fb29d1339
Royaldotcon <rr6216009@gmail.com>
2025-05-12 18:45:08 +0530

    Update navbar.tsx

--- components/main/navbar.tsx
+++ components/main/navbar.tsx
@@ -41,15 +41,7 @@ export const Navbar = () => {
               </Link>
             ))}
 
-            {/* About Me */}
-            <Link
-              href={LINKS.sourceCode}
-              target="_blank"
-              rel="noreferrer noopener"
-              className="cursor-pointer hover:text-[rgb(112,66,248)] transition"
-            >
-              About Me
-            </Link>
+            
           </div>
         </div>
 
