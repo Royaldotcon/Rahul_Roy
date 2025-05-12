ca9f6ea3d9df2021aaadfd4472384d3a1cb018f7
Royaldotcon <rr6216009@gmail.com>
2025-05-12 21:20:33 +0530

    Update navbar.tsx

--- components/main/navbar.tsx
+++ components/main/navbar.tsx
@@ -18,7 +18,7 @@ export const Navbar = () => {
           className="flex items-center"
         >
           <Image
-            src="/logo1.png"
+            src="/logo.png"
             alt="Logo"
             width={70}
             height={70}
