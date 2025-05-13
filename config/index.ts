5e3a7f7c5cb5358fdfc433338290c94972a98afb
Royaldotcon <rr6216009@gmail.com>
2025-05-13 23:24:10 +0530

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -9,7 +9,7 @@ export const siteConfig: Metadata = {
     url: "https://rahul-royal.vercel.app", // or your custom domain
     images: [
       {
-        url: "/project-3.png", // or full URL like https://yourdomain.com/og-image.png
+        url: "/og.png", // or full URL like https://yourdomain.com/og-image.png
         width: 1200,
         height: 630,
         alt: "Rahul Roy Portfolio Preview",
