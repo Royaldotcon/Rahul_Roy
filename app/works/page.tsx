2ee7cd4e6b46e7c2d82c0db8d4dbdd60e5765fd8
Royaldotcon <rr6216009@gmail.com>
2025-05-15 00:04:34 +0530

    Update page.tsx

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -1,35 +1,103 @@
-import { SparklesIcon } from "@heroicons/react/24/solid";
-import { motion } from "framer-motion";
-import Image from "next/image";
+import { ProjectCard } from "@//components/sub/project-card";
+import { PROJECTS } from "@//constants";
 
-export default function AboutPage() {
+const Projects = () => {
   return (
-    <main className="p-8 text-white">
-      {/* Title and Introduction Section */}
-      <br /><br />
-     
-     
-
-      <div className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]">
-        {/*<img
-          src="/pic.jpg"
-          alt="Rahul Roy"
-          className="w-44 h-44 rounded-full object-cover mx-300 border-4 border-transparent bg-gradient-to-r from-purple-500 to-cyan-500 p-[2px]"
-        />*/}
+    <section
+      id="projects"
+      className="flex flex-col items-center justify-center py-20"
+    >
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+        Projects
+      </h1>
+      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+        {PROJECTS.map((project) => (
+          <ProjectCard
+            key={project.title}
+            src={project.image}
+            title={project.title}
+            description={project.description}
+            link={project.link}
+          />
+        ))}
       </div>
-
-      {/* Profile Information */}
-      <div className="flex flex-col gap-6 mt-6 text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl max-w-full w-full h-auto">
-        <span>
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 text-5xl sm:text-4xl md:text-6xl lg:text-6xl">
-            Comming Soon!!
-          </span>
-          <br />
-          
-        </span>
-      </div>
-
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+        Graphic Designs
+      </h1>
+      <div className="grid grid-cols-2 md:grid-cols-5 gap-5 px-3 max-w-6xl mx-auto">
+ 
+  <img
+    src="https://www.texavision.in/_next/image?url=%2Fdj.png&w=1080&q=75"
+    alt="Rahul Roy"
+    className="w-full h-auto object-cover border-4 border-transparent bg-gray-900 p-[2px]"
+  />
+<img
+    src="graphics/THINKFAST.png"
+    alt="Rahul Roy"
+    className="w-full h-auto object-cover border-4 border-transparent bg-gray-900 p-[2px]"
+  /><img
+    src="graphics/texa1.png"
+    alt="Rahul Roy"
+    className="w-full h-auto object-cover border-4 border-transparent bg-gray-900 p-[2px]"
+  />
+ <img
+    src="/graphicS/innova.png"
+    alt="Rahul Roy"
+    className="w-full h-auto object-cover border-4 border-transparent bg-gray-900 p-[2px]"
+  />
+ <img
+    src="/graphics/unscripted.png"
+    alt="Rahul Roy"
+    className="w-full h-auto object-cover border-4 border-transparent bg-gray-900 p-[2px]"
+  /> <img
+    src="https://www.texavision.in/_next/image?url=%2Fbohur.png&w=1080&q=75"
+    alt="Rahul Roy"
+    className="w-full h-auto object-cover border-4 border-transparent bg-gray-900 p-[2px]"
+  />
   
-    </main>
+  <img
+    src="https://www.texavision.in/_next/image?url=%2Freunion.png&w=1080&q=75"
+    alt="Rahul Roy"
+    className="w-full h-auto object-cover border-4 border-transparent bg-gray-900 p-[2px]"
+  />
+  <img
+    src="https://www.texavision.in/_next/image?url=%2FTEXAWALK.png&w=1080&q=75"
+    alt="Rahul Roy"
+    className="w-full h-auto object-cover border-4 border-transparent bg-gray-900 p-[2px]"
+  />
+  <img
+    src="https://www.texavision.in/_next/image?url=%2Ffresh.png&w=1080&q=75"
+    alt="Rahul Roy"
+    className="w-full h-auto object-cover border-4 border-transparent bg-gray-900 p-[2px]"
+  />
+  <img
+    src="https://www.texavision.in/_next/image?url=%2Ffarewell.png&w=1080&q=75"
+    alt="Rahul Roy"
+    className="w-full h-auto object-cover border-4 border-transparent bg-gray-900 p-[2px]"
+  />
+   
+
+  </div><br />
+{/* Landscape Images */}
+<div className="grid grid-cols-1 md:grid-cols-2 gap-5 px-3 max-w-6xl mx-auto">
+  <img
+    src="/graphics/textrivia.png"
+    alt="Rahul Roy"
+    className="w-full h-auto object-cover border-4 border-transparent bg-gray-900 p-[2px]"
+  />
+<img
+    src="/graphics/rangbahar.png"
+    alt="Rahul Roy"
+    className="w-full h-auto object-cover border-4 border-transparent bg-gray-900 p-[2px]"
+  /><img
+    src="/graphics/TEXATECH 4BY3.png"
+    alt="Rahul Roy"
+    className="w-full h-auto object-cover border-4 border-transparent bg-gray-900 p-[2px]"
+  />
+  </div>
+
+    </section>
   );
-}
+};
+
+export default Projects;
