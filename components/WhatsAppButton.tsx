f3b8e74b5be2af8321e96b16ab66855f2ecd1d8e
Royaldotcon <rr6216009@gmail.com>
2025-05-15 19:46:31 +0530

    commit

--- components/WhatsAppButton.tsx
+++ components/WhatsAppButton.tsx
@@ -8,7 +8,7 @@ const WhatsAppButton: React.FC = () => {
       href="https://wa.me/918250084850"
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed bottom-6 right-6 bg-gradient-to-r from-green-500 to-magenta-800 hover:from-green-600 hover:to-purple-600  text-white p-2 rounded-full shadow-lg z-50 transition-transform duration-300 hover:scale-100"
+      className="fixed bottom-6 right-6 bg-gradient-to-r from-purple-800 to-cyan-400 hover:from-green-600 hover:to-purple-600  text-white p-2 rounded-full shadow-lg z-50 transition-transform duration-300 hover:scale-100"
       aria-label="Chat on WhatsApp"
     >
       <FaWhatsapp className="text-2xl" />
