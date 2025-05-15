85441c9d4b6f24219ccb97a55f2a2776f390624d
Royaldotcon <rr6216009@gmail.com>
2025-05-16 00:12:33 +0530

    commit

---
+++ lib/blogsData.ts
@@ -0,0 +1,7 @@
+  {
+    id: '1',
+    title: 'How Aitihya Was Born',
+    author: 'Rahul Roy',
+    description: 'A story about how Aitihya began its journey to preserve Bengal’s tradition.',
+    content: 'Full blog content about Aitihya and its inception...'
+  },
