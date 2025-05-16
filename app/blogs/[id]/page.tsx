f66f753bd67aecc2c18fdff6c9b2e657869c154f
Royaldotcon <rr6216009@gmail.com>
2025-05-16 12:10:33 +0530

    commit

--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -1,3 +1,9 @@
+"use client";
+import { motion } from "framer-motion";
+import {  slideInFromLeft,
+  slideInFromRight,
+  slideInFromTop } from "@/lib/motion";
+
 import { blogs } from '@/lib/blogsData'
 import { notFound } from 'next/navigation'
 
@@ -13,10 +19,22 @@ export default function BlogPage({ params }: Params) {
   if (!blog) return notFound()
 
   return (
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      className="flex flex-row items-center justify-center px-0 mt-20 w-full z-[20]"
+    >
     <main className="max-w-full mx-auto p-20"><br />
-      <h1 className="text-3xl text-white font-bold mb-2">{blog.title}</h1>
-      <p className="text-gray-300 mb-4"><i>By <strong>{blog.author}</strong></i></p>
-      <p className="text-lg text-justify text-gray-400">{blog.content}</p>
+      <motion.h1 
+      variants={slideInFromTop}
+      className="text-3xl text-white font-bold mb-2">{blog.title}</motion.h1>
+      <motion.p 
+      variants={slideInFromLeft(0.6)}
+      className="text-gray-300 mb-4"><i>By <strong>{blog.author}</strong></i></motion.p>
+      <motion.p
+      variants={slideInFromLeft(0.8)}
+      className="text-lg text-justify text-gray-400">{blog.content}</motion.p>
     </main>
+    </motion.div>
   )
 }
--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,31 +1,49 @@
+"use client";
+import { motion } from "framer-motion";
+import {  slideInFromLeft,
+  slideInFromRight,
+  slideInFromTop } from "@/lib/motion";
 import Link from 'next/link'
 import { blogs } from '@/lib/blogsData'
 
 export default function BlogsPage() {
   return (
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      className="flex flex-row items-center justify-center px-0 mt-20 w-full z-[20]"
+    >
     <main className="max-w-full mx-auto p-20">
       <br />
-      <div className="flex items-center justify-between mb-6">
+      <motion.div
+      variants={slideInFromTop}
+      className="flex items-center justify-between mb-6">
         <h1 className="text-3xl text-cyan-400 font-bold">All Blogs</h1>
         
-      </div>
+      </motion.div>
 
       <ul className="space-y-6">
         {blogs.map((blog) => (
           <li key={blog.id} className="border p-4 rounded shadow">
-            <Link 
+            <motion.Link
+            variants={slideInFromLeft(0.7)} 
               href={`/blogs/${blog.id}`} 
               className="text-xl font-semibold text-gray-200 hover:underline"
             >
               {blog.title}
-            </Link>
-            <p className="text-gray-300">
+            </motion.Link>
+            <motion.p
+            variants={slideInFromLeft(0.9)}
+            className="text-gray-300">
               <i>By <strong>{blog.author}</strong></i>
-            </p>
-            <p className="text-gray-400 text-justify mt-2">{blog.description}</p>
+            </motion.p>
+            <motion.p 
+            variants={slideInFromLeft(1)}
+            className="text-gray-400 text-justify mt-2">{blog.description}</motion.p>
           </li>
         ))}
       </ul>
     </main>
+    </motion.div>
   )
 }
