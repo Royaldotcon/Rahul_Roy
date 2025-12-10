dab31124009ff51c5a3c00512bb8bfa5a333dfe0
Royaldotcon <rr6216009@gmail.com>
2025-12-11 00:36:47 +0530

    project section error rebuild

--- components/main/projects.tsx
+++ components/main/projects.tsx
@@ -1,5 +1,5 @@
 import { ProjectCard } from "@/components/sub/project-card";
-import { PROJECTS } from "@/constants";
+import { PROJECTS, PROJECTS2 } from "@/constants";
 
 export const Projects = () => {
   return (
